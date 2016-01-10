ffdceecf6e980c7eefae7057cada68e1650b7201
FunkySamuel37 <masterzhiwei@gmail.com>
2016-01-11 00:41:45 +0800

    add annotations for fileReader

--- client/imgUpload.js
+++ client/imgUpload.js
@@ -17,25 +17,28 @@
 //     });
 //   }
 // });
-var dataString;
 
 Template.imgUploader.events ({
   'change #myFileInput': function(e, template) {
+      // 读取所有files
       var files=event.target.files;
       if(files.length===0){
         return;
       }
+      // WARN: 选取file的第一个，以后需要循环所有图片
       var file=files[0];
       console.log(file);
 
-      //
+      // 初始化FileReader来读取file
       var fileReader=new FileReader();
+      // callback: FileReader.onloadend 文件读取完毕时调用
       fileReader.onloadend = function () {
-        dataString = fileReader.result;
+        var dataString = fileReader.result;
+        // WARN: 仅限一个图片时使用，后期需要修改
         document.querySelector('img').src = dataString;
-        // $('[name=image]').src = dataString;
         console.log(dataString);
       };
+      // 转换成base64
       fileReader.readAsDataURL(file);
    },
 });
