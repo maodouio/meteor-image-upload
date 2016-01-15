dc1f20b8da9af085ed4099d58f47332033629ecb
FunkySamuel37 <masterzhiwei@gmail.com>
2016-01-15 20:50:27 +0800

    modified image uploaded callback function, Session.set 'logoImage' for image url

--- client/imgUpload.js
+++ client/imgUpload.js
@@ -1,39 +1,52 @@
-Meteor.call("getQiniuDomain", function(error, result){
-  if(result){
+Meteor.call("getQiniuDomain", function(error, result) {
+  if (result) {
     Session.set("qiniuUploadDomain", result);
   }
 });
-Template.imgUploader.events ({
+Template.imgUploader.onRendered = function() {
+
+}
+
+Template.imgUploader.events({
+  'click #fileUpload': function() {
+    $('#myFileInput').trigger("click");
+  },
+
   'change #myFileInput': function(e, template) {
-      // 读取所有files
-      var files=event.target.files;
-      if(files.length===0){
-        return;
-      }
-      // WARN: 选取file的第一个，以后需要循环所有图片
-      var file=files[0];
-      // console.log(file);
+    // 读取所有files
+    var files = event.target.files;
+    if (files.length === 0) {
+      return;
+    }
+    // WARN: 选取file的第一个，以后需要循环所有图片
+    var file = files[0];
+    // console.log(file);
 
-      // 初始化FileReader来读取file
-      var fileReader=new FileReader();
-      // callback: FileReader.onloadend 文件读取完毕时调用
-      fileReader.onloadend = function () {
-        var dataString = fileReader.result;
-        // WARN: 仅限一个图片时使用，后期需要修改
-        // console.log(dataString);
+    // 初始化FileReader来读取file
+    var fileReader = new FileReader();
+    // callback: FileReader.onloadend 文件读取完毕时调用
+    fileReader.onloadend = function() {
+      var dataString = fileReader.result;
+      // WARN: 仅限一个图片时使用，后期需要修改
+      // console.log(dataString);
 
-        var dataUrl = dataString;
-        Meteor.call('sendAvatarInBase64', dataUrl, function(err, res) {
-          if (res) {
-            document.querySelector('img').src = Session.get("qiniuUploadDomain") + res;
-            alert('图片上传成功');
-          }else {
-            alert('图片上传失败，请重试');
-          }
-        });
+      var dataUrl = dataString;
+      Meteor.call('sendAvatarInBase64', dataUrl, function(err, res) {
+        if (res) {
+          // document.querySelector('img').src = Session.get("qiniuUploadDomain") + res;
+          $("#imageReveal").attr("src", (Session.get(
+            "qiniuUploadDomain") + res));
+          Session.set("logoImage", (Session.get(
+            "qiniuUploadDomain") + res));
+          // console.log(Session.get("qiniuUploadDomain") + res);
+          alert('图片上传成功');
+        } else {
+          alert('图片上传失败，请重试');
+        }
+      });
 
-      };
-      // 转换成base64
-      fileReader.readAsDataURL(file);
-   },
+    };
+    // 转换成base64
+    fileReader.readAsDataURL(file);
+  },
 });
