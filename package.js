8b3a776fadbbdd3b61b8ea2742fcfebe3e4d67d4
limingth <limingth@gmail.com>
2016-03-23 11:55:45 +0800

    Relese 0.0.1

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'maodouio:meteor-image-upload',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Upload image to qiniu cloud',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/maodouio/meteor-image-upload',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
